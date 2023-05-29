0b3911a79ba0f978cf7793b3afbbff8f1fca7453
jasmine0714 <woooooooow22@gmail.com>
2023-05-29 21:03:27 +0900

    💄 Header style

---
+++ src/common/components/Header/HeaderStyle.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  position: sticky;
+  display: flex;
+  justify-content: flex-end;
+  height: 57px;
+  background-color: #2d2929;
+`;
+
+export const Hamburger = styled.img`
+  padding: 20px 0px 30px;
+  margin-right: 18px;
+  height: 70px;
+  width: 30px;
+`;
