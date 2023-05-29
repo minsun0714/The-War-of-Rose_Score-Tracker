a3b784f9c5a3c36474fa3badd68de2fd2053b8d5
jasmine0714 <woooooooow22@gmail.com>
2023-05-29 22:02:14 +0900

    💄 Font Inika 적용

--- src/common/components/style.tsx
+++ src/common/components/style.tsx
@@ -1,12 +1,21 @@
 import { createGlobalStyle } from "styled-components";
+import { FontURL } from "../type/type";
 
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+  font-family: 'Inika';
+  src: url(${FontURL.FontInika}) format('truetype');
+    }
+
  *{
     box-sizing: border-box;
     margin: 0;
+    font-family: 'Inika', sans-serif;
+
  }
+
  body{
-   background-image: url("/rose background.png");
+   background-image: url("/rose background.png") ;
  }
 `;
 
