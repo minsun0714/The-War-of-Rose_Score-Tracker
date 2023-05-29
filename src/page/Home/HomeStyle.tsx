d0cc42dc343e18620cc5c9ca29192ee943769e65
jasmine0714 <woooooooow22@gmail.com>
2023-05-29 22:46:06 +0900

    🎨 google login 버튼 생성

--- src/page/Home/HomeStyle.tsx
+++ src/page/Home/HomeStyle.tsx
@@ -11,3 +11,26 @@ export const GameTitle = styled.span`
   font-size: 40px;
   color: #e1d8d8;
 `;
+
+export const GoogleLoginContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 60px;
+`;
+
+export const GoogleLogoBtn = styled.button`
+  background-color: white;
+  border: none;
+  padding: 25.5px;
+  background-image: url("/google_logo.png");
+`;
+export const GoogleLoginBtn = styled.button`
+  background-color: #4285f4;
+  height: 51px;
+  width: 200px;
+  color: white;
+  border: none;
+  font-family: Inter;
+  box-shadow: 1px 5px 3px rgba(0, 0, 0, 0.3);
+`;
