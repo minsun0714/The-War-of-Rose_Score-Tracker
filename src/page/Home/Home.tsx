ab81d5451c4cc3409f15fc35f504a3e0cf44783c
jasmine0714 <woooooooow22@gmail.com>
2023-05-29 22:03:25 +0900

    🎨 Home 컴포넌트에 앱 title 렌더링

---
+++ src/page/Home/Home.tsx
@@ -0,0 +1,11 @@
+import { GameTitleContainer, GameTitle } from "./HomeStyle";
+
+function Home() {
+  return (
+    <GameTitleContainer>
+      <GameTitle>The War of Rose</GameTitle>
+      <GameTitle>Score Tracker</GameTitle>
+    </GameTitleContainer>
+  );
+}
+export default Home;
---
+++ src/page/Home/HomeStyle.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const GameTitleContainer = styled.label`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+`;
+
+export const GameTitle = styled.span`
+  font-size: 40px;
+  color: #e1d8d8;
+`;
