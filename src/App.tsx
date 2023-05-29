ade2fb9a72d112b957b8785de62cba4cc9e7b036
jasmine0714 <woooooooow22@gmail.com>
2023-05-29 22:01:54 +0900

    🚚  Header, Home 컴포넌트에 React router 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./common/components/Header/Header";
+import Home from "./page/Home/Home";
 
 function App() {
-  return <Header />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
