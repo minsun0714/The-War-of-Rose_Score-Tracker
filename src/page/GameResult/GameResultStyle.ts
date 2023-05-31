f4980258f4123c162bc4cf5d23069bd4edceae54
jasmine0714 <woooooooow22@gmail.com>
2023-05-31 17:03:50 +0900

    💄 GameResult style 파일 생성

---
+++ src/page/GameResult/GameResultStyle.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+import { StartButtonContainer, StartBtn } from "../MyPage/MyPageStyle";
+
+export const GameBoardWrapper = styled.div`
+  background-color: #e1d8d8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 22px;
+`;
+
+export const GameBoardElement = styled.td`
+  padding: 1.5px 12px 1.5px 12px;
+  background-image: linear-gradient(20deg, #955959, #813f3f);
+  border-radius: 5px;
+  color: transparent;
+`;
+
+export const UserWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+  font-size: 30px;
+`;
+export const UserName = styled.span`
+  margin-left: 15px;
+`;
+
+export const ReloadWrapper = styled.div`
+  position: absolute;
+  margin-left: 20px;
+  padding-top: 12px;
+`;
+
+export const GetScoreBtnWrapper = styled(StartButtonContainer)`
+  margin: 0;
+`;
+
+export const GetScoreBtn = styled(StartBtn)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(200deg, #eec23d, #fbbc05);
+  color: black;
+  width: 250px;
+  :active {
+    background-image: linear-gradient(0deg, #fbbc05, #ecf23d);
+  }
+`;
+
+export const ReloadImg = styled.img``;
+
+export const Crown = styled.img`
+  margin: 0 30px 0 -60px;
+`;
+
+export const UserImg = styled.img`
+  height: 35px;
+`;
+
+export const GameBoard = styled.table``;
+
+export const GameCell = styled.tr``;
