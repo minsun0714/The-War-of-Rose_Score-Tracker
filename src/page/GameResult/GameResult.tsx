e113bd65e02369c49f35427e2d1a0afd35fcef66
jasmine0714 <woooooooow22@gmail.com>
2023-05-31 21:03:38 +0900

    ⚡️ redux store에서 가져온 username으로 대체

--- src/page/GameResult/GameResult.tsx
+++ src/page/GameResult/GameResult.tsx
@@ -8,16 +8,20 @@ import { GameResultImgURL } from "../../common/type/enum";
 import GameBoardContainer from "./GameBoard";
 import User from "./GameUser";
 import { GET_SCORE_BUTTON_PHRASE } from "../../common/constant";
+import { useSelector } from "react-redux";
 
 function GameResult() {
+  const userNames = useSelector(
+    (store: { userName1: string; userName2: string }) => store
+  );
   return (
     <>
       <ReloadWrapper>
         <ReloadImg src={GameResultImgURL.reloadImg}></ReloadImg>
       </ReloadWrapper>
-      <User src={GameResultImgURL.user1} userNumber={1} />
+      <User src={GameResultImgURL.user1} userName={userNames.userName1} />
       <GameBoardContainer />
-      <User src={GameResultImgURL.user2} userNumber={2} />
+      <User src={GameResultImgURL.user2} userName={userNames.userName2} />
       <GetScoreBtnWrapper>
         <GetScoreBtn>{GET_SCORE_BUTTON_PHRASE}</GetScoreBtn>
       </GetScoreBtnWrapper>
