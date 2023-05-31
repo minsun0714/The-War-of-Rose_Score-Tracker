49490a428909dda1d7855b22c87a477c376a039d
jasmine0714 <woooooooow22@gmail.com>
2023-05-31 17:00:05 +0900

    🚚 enum 파일명 변경

---
+++ src/common/type/enum.ts
@@ -0,0 +1,14 @@
+export const enum HamburgerURL {
+  Img = "/🦆 icon _menu_.png",
+}
+
+export const enum FontURL {
+  FontInika = "/Inika/Inika-Regular.ttf",
+}
+
+export const enum GameResultImgURL {
+  reloadImg = "/reload_.png",
+  user1 = "/user1.png",
+  user2 = "/user2.png",
+  crown = "/crown.png",
+}
