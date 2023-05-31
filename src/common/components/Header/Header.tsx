a666041b8e38cfa369f950d40d9081f1fb34d037
jasmine0714 <woooooooow22@gmail.com>
2023-05-31 16:58:56 +0900

    🚚 enum 파일명 변경

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
 import { StyledHeader, Hamburger } from "./HeaderStyle";
-import { HamburgerURL } from "../../type/type";
+import { HamburgerURL } from "../../type/enum";
 
 function Header() {
   return (
