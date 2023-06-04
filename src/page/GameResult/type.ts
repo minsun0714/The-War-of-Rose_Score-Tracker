6c22e42c95414acc885873643713d13b5718efc8
jasmine0714 <woooooooow22@gmail.com>
2023-06-04 19:45:58 +0900

    🏷️ prop 추가에 따라 UserProp에도 userNumber 추가

--- src/page/GameResult/type.ts
+++ src/page/GameResult/type.ts
@@ -2,7 +2,10 @@ import { GameResultImgURL } from "../../common/type/enum";
 
 export type UserImg = GameResultImgURL.user1 | GameResultImgURL.user2;
 
+type UserNumber = 1 | 2;
+
 export type UserProp = {
+  userNumber: UserNumber;
   src: UserImg;
-  userName: string;
+  userName?: string;
 };
