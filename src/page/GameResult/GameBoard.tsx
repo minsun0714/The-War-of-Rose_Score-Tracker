4bb8a3e8385a65cf3fe22682cc477e389aceb6db
jasmine0714 <woooooooow22@gmail.com>
2023-06-04 23:08:34 +0900

    🎨 보드판 숫자 클릭 시 user의 tap 순서에 맞게 변하도록 구현

--- src/page/GameResult/GameBoard.tsx
+++ src/page/GameResult/GameBoard.tsx
@@ -5,9 +5,19 @@ import {
   GameBoardRow,
 } from "./GameResultStyle";
 import { GameResultStyle } from "../../common/style/style";
+import { useDispatch, useSelector } from "react-redux";
+import { getGameBoard } from "../../store/BoardStore";
+import { RootState } from "../../store/rootStore";
 
 function GameBoardContainer() {
-  const table = [...Array(9)].map((_) => [...Array(9)].map((_) => 0));
+  const dispatch = useDispatch();
+  const table = useSelector((store: RootState) => store.gameBoardStore);
+  const tapTurn = useSelector((store: RootState) => store.tapTurnStore.tapTurn);
+
+  const onClick = (rowIdx: number, columnIdx: number) => {
+    dispatch(getGameBoard({ currentUser: tapTurn, rowIdx, columnIdx }));
+  };
+
   return (
     <GameBoardWrapper>
       <GameResultStyle />
@@ -16,8 +26,13 @@ function GameBoardContainer() {
           return (
             <tbody key={rowIdx}>
               <GameBoardRow>
-                {row.map((element: number, elementIdx: number) => (
-                  <GameBoardCell key={elementIdx}>0</GameBoardCell>
+                {row.map((element: number, columnIdx: number) => (
+                  <GameBoardCell
+                    key={rowIdx + "" + columnIdx}
+                    onClick={() => onClick(rowIdx, columnIdx)}
+                  >
+                    {element}
+                  </GameBoardCell>
                 ))}
               </GameBoardRow>
             </tbody>
