c8cf641f28711b32c653b139ec961cc291f5205e
jasmine0714 <woooooooow22@gmail.com>
2023-06-04 23:07:52 +0900

    ✨ game board를 전역 상태로 관리

--- src/store/rootStore.tsx
+++ src/store/rootStore.tsx
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { userNames } from "./usernameStore";
 import { tapTurn } from "./TapTurnStore";
+import { GameBoard } from "./BoardStore";
 
 const rootReducer = combineReducers({
   userNames: userNames.reducer,
   tapTurnStore: tapTurn.reducer,
+  gameBoardStore: GameBoard.reducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
