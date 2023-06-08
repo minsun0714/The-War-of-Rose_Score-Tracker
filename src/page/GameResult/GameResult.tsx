42763e931c0712598b58e10ab2ef528948c725cb
jasmine0714 <woooooooow22@gmail.com>
2023-06-08 21:39:27 +0900

    ✨ 게임 결과를 보여주는 모달창 구현

--- src/page/GameResult/GameResult.tsx
+++ src/page/GameResult/GameResult.tsx
@@ -1,20 +1,35 @@
+import { useState } from "react";
 import { GetScoreBtnWrapper, GetScoreBtn } from "./GameResultStyle";
+import { RootState } from "../../store/rootStore";
 import { GameResultImgURL } from "../../common/type/enum";
 import GameBoardContainer from "./GameBoard";
 import User from "./GameUser";
 import { GET_SCORE_BUTTON_PHRASE } from "../../common/constant";
 import { useSelector } from "react-redux";
-import { RootState } from "../../store/rootStore";
 import { useGetScore } from "../../common/customHooks/useGetScore";
+import {
+  ScoreTitleWrapper,
+  ScoreTitle,
+  ScoreWrapper,
+  AvatarImgScore,
+  UserScoreWrapper,
+  UserNameModal,
+  Score,
+} from "./GameResultStyle";
+import { Modal, ActionsWrapper, ActionBtn } from "../MyPage/MyPageStyle";
 
 function GameResult() {
   const userNames = useSelector((store: RootState) => store.userNames);
   const table = useSelector((store: RootState) => store.gameBoardStore);
   const finalScore = useGetScore(table);
   const onClickShowResult = () => {
-    console.log("최종 점수", finalScore);
+    setOpen(true);
   };
 
+  const [open, setOpen] = useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
     <>
       <User
@@ -33,6 +48,29 @@ function GameResult() {
           {GET_SCORE_BUTTON_PHRASE}
         </GetScoreBtn>
       </GetScoreBtnWrapper>
+
+      <Modal open={open} onClose={handleClose}>
+        <ScoreTitleWrapper>
+          <ScoreTitle>Final Score</ScoreTitle>
+        </ScoreTitleWrapper>
+        <ScoreWrapper>
+          <AvatarImgScore src={GameResultImgURL.user1}></AvatarImgScore>
+          <UserScoreWrapper>
+            <UserNameModal>{userNames.userName1}</UserNameModal>
+            <Score>{finalScore.user1}</Score>
+          </UserScoreWrapper>
+        </ScoreWrapper>
+        <ScoreWrapper>
+          <AvatarImgScore src={GameResultImgURL.user2}></AvatarImgScore>
+          <UserScoreWrapper>
+            <UserNameModal>{userNames.userName2}</UserNameModal>
+            <Score>{finalScore.user2}</Score>
+          </UserScoreWrapper>
+        </ScoreWrapper>
+        <ActionsWrapper>
+          <ActionBtn onClick={handleClose}>ok</ActionBtn>
+        </ActionsWrapper>
+      </Modal>
     </>
   );
 }
--- src/page/GameResult/GameResultStyle.ts
+++ src/page/GameResult/GameResultStyle.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import { StartButtonContainer, StartBtn } from "../MyPage/MyPageStyle";
+import { AvatarImg, UserNameWrapper } from "../MyPage/MyPageStyle";
 import { UserWrpperProps, GameBoardCellProps } from "./type";
+import DialogContent from "@mui/material/DialogContent";
 
 export const GameBoardWrapper = styled.div`
   background-color: #e1d8d8;
@@ -73,4 +75,48 @@ export const GameBoardCell = styled.td<GameBoardCellProps>`
   }};
   border-radius: 5px;
   color: transparent;
+`;
+
+export const ScoreTitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 32px;
+  height: 45px;
+  margin-top: 10px;
+`;
+
+export const ScoreTitle = styled(DialogContent)`
+  display: flex;
+  flex-direction: row;
+  background-color: #955959;
+  color: #e1d8d8;
+`;
+
+export const ScoreWrapper = styled(UserNameWrapper)`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const UserScoreWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: -10px;
+`;
+
+export const AvatarImgScore = styled(AvatarImg)`
+  height: 40px;
+  width: 44px;
+`;
+
+export const UserNameModal = styled.span`
+  font-size: 14px;
+  margin-top: -8px;
+`;
+
+export const Score = styled.span`
+  font-size: 30px;
+  margin-left: 30px;
+  margin-top: -8px;
+  text-decoration: none;
 `;
